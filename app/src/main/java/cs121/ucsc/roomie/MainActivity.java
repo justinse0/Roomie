014d7dd1a07bf6d94d34ce4c9b814d91dab5b32d
yli186 <likevin111@gmail.com>
2017-10-31 15:52:45 -0700

    login page UI

--- app/src/main/java/cs121/ucsc/roomie/MainActivity.java
+++ app/src/main/java/cs121/ucsc/roomie/MainActivity.java
@@ -1,13 +1,19 @@
 package cs121.ucsc.roomie;
+import android.support.v7.app.AppCompatActivity;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
 }
--- app/src/main/java/cs121/ucsc/roomie/SimpleLoginActivity.java
+++ app/src/main/java/cs121/ucsc/roomie/SimpleLoginActivity.java
@@ -1,13 +1,24 @@
 package cs121.ucsc.roomie;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class SimpleLoginActivity extends AppCompatActivity {
+public class SimpleLoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_login);
+        final Button login =  findViewById(R.id.login);
+        final Button newuser =  findViewById(R.id.newaccount);
+
+        final EditText account = findViewById(R.id.account);
+        final EditText password = findViewById(R.id.password);
+
+
+
+
     }
 }
