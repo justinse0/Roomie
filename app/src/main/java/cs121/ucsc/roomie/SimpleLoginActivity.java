d9b0b13bd4d7cf3f1b9f3eb176ff3de2dffe4d7f
yli186 <likevin111@gmail.com>
2017-10-31 16:21:55 -0700

    added activities

--- app/src/main/java/cs121/ucsc/roomie/SimpleLoginActivity.java
+++ app/src/main/java/cs121/ucsc/roomie/SimpleLoginActivity.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import static android.R.attr.button;
+
 
 public class SimpleLoginActivity extends Activity {
 
