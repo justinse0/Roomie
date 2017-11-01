e087e6748b1ba41c0a7d757a35d812647f04d4ba
yli186 <likevin111@gmail.com>
2017-10-31 23:20:16 -0700

    added model, skeleton

--- app/src/main/java/cs121/ucsc/roomie/Model.java
+++ app/src/main/java/cs121/ucsc/roomie/Model.java
@@ -1,8 +1,49 @@
 package cs121.ucsc.roomie;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Observable;
 /**
- * Created by user on 10/31/2017.
+ * Created by kevin on 10/31/2017.
  */
 
 public class Model extends Observable{
+    private List<String> accounts;
+    private List<String> passwords;
+    private List<String> names;
+
+    public Model(){
+        accounts = new ArrayList<String>();
+        passwords = new ArrayList<String>();
+    }
+    //search through the list of accounts, see if there's a match, return 1 if match, 0 no match.
+    public int SearchForAccount(String acc){
+        return 0;
+    }
+    //search through the list of passwords, see if there's a match, return 1 if match, 0 no match.
+    public int SearchForPassword(String pass){
+        return 0;
+    }
+    //add the new account that user entered to the list of accounts
+    //add the new password that user entered to the list of passwords
+    public void AddNewAccount(String acc, String pass){
+
+    }
+
+    //add the new legal name that user entered to the list of names
+    public void AddNewName(String nam){
+
+    }
+
+    //.... follow this pattern when getting new user.
+
+
+    // if login succeed
+    public void ToMainScreen(){
+
+    }
+    // if user choose to create new
+    public void ToNewUser(){
+
+    }
+
 }
--- app/src/main/java/cs121/ucsc/roomie/SimpleLoginActivity.java
+++ app/src/main/java/cs121/ucsc/roomie/SimpleLoginActivity.java
@@ -6,38 +6,62 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.Observable;
+import java.util.Observer;
 
 
-public class SimpleLoginActivity extends Activity {
+public class SimpleLoginActivity extends Activity implements Observer, Button.OnClickListener{
 
+    private Model model;
+    private Button login;
+    private Button newuser;
+    private EditText account;
+    private EditText password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_login);
-        final Button login =  findViewById(R.id.login);
-        final Button newuser =  findViewById(R.id.newaccount);
+        login = (Button)findViewById(R.id.login);
+        newuser = (Button) findViewById(R.id.newaccount);
 
-        final EditText account = findViewById(R.id.account);
-        final EditText password = findViewById(R.id.password);
+        account = (EditText) findViewById(R.id.account);
+        password = (EditText) findViewById(R.id.password);
 
-        login.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                //fetch login data
-                //compare user input with database
-                //if match, direct to main page
-                //else do nothing, toast user not found / password/account incorrect
-            }
-        });
+        login.setOnClickListener(this);
+        newuser.setOnClickListener(this);
 
-
-        newuser.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                //direct to NewUserActivity that ask user to create account / get basic info
-            }
-        });
+        model = new Model();
+        model.addObserver(this);
 
 
 
 
+    }
+
+    @Override
+    public void update(Observable observable, Object o){
+
+    }
+    //LOGIN
+    //fetch login data (gettext)
+    //compare user input with database (if ==1)
+    //if match, direct to main page
+    //else do nothing, toast user not found / password/account incorrect
+
+    //NEWACCOUNT
+    //direct to NewUserActivity that ask user to create account / get basic info
+    @Override
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.login:
+                if((model.SearchForAccount(account.getText().toString())  == 1) && (model.SearchForPassword(password.getText().toString()) == 1))
+                {
+                    model.ToMainScreen();
+                }
+                break;
+            case R.id.newaccount:
+                model.ToNewUser();
+                break;
+        }
     }
 }
