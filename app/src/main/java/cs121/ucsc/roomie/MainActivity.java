6065b880605496114b13f4f30fbbbfbcbec23de9
abuderi <aabuderi@ucsc.edu>
2017-11-20 15:27:37 -0800

    edited mainActivity with database support

--- app/src/main/java/cs121/ucsc/roomie/MainActivity.java
+++ app/src/main/java/cs121/ucsc/roomie/MainActivity.java
@@ -1,7 +1,6 @@
 package cs121.ucsc.roomie;
 import android.app.Activity;
 import android.graphics.PorterDuff;
-import android.support.v7.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,115 +9,127 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
-import com.sendbird.android.SendBird;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends Activity {
     private final String APPID = new String("A21A2BDC-6192-4A27-A4A9-F3FEA23F2CFA");
-    private List<User> roomies;
+    private ArrayList<String> roomies;
     private ListView listView;
-    private ArrayList<String> userList;
+    private ArrayList<User> houseUserList;
     private User currUser;
+    private String userPass;
     private boolean secondPress;
     private String[] userArray;
     Button displayMates ;
     Button goTodo;
-    Button messageStart  ;
+    Button messageStart;
     public static String[] staticNames;
+    FirebaseAuth mAuth;
+    DatabaseReference database;
+    private int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String[] staticNames = {"John Cena",
-                "Alan Rickman",
-                "Olivia Newton-John",
-                "Aziz Ansari",
-                "Mindy Kaling"};
-        //staticNames = new String[5];
+        roomies  = new ArrayList<String>();
+        counter=0;
         listView = new ListView(this);
-       // roomies = SimpleLoginActivity.getUsers();
-        userList = new ArrayList<String>();
-        String username = getIntent().getStringExtra("CurrentUser");
+        houseUserList = new ArrayList<User>();
+        userPass = new String(getIntent().getStringExtra("CurrentUser"));
+
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance().getReference();
+
+
+
         //initialize the connection to SendBird servers
        // SendBird.init(APPID, this);
 
         //create the list and buttons for displaying roommates
         //and find the current user
         listView = (ListView) findViewById(R.id.roomieList);
-         displayMates = (Button) findViewById(R.id.roomies);
-         goTodo = (Button) findViewById(R.id.todo);
-         messageStart = (Button) findViewById(R.id.messaging);
+        displayMates = (Button) findViewById(R.id.roomies);
+        goTodo = (Button) findViewById(R.id.todo);
+        messageStart = (Button) findViewById(R.id.messaging);
         secondPress = false;
-       /* for(int i=0; i<roomies.size();i++){
-            if(roomies.get(i).username.equals(username)){
-                currUser = roomies.get(i);
-                break;
-            }
-        }/*
-        int counter=0;
-        for(int i=0; i<roomies.size(); i++){
-            if(!(roomies.get(i).username.equals(currUser.username)) &&
-                    roomies.get(i).houseName == currUser.houseName){
-                userList.add(roomies.get(i).username);
-                counter+=1;
-            }
-        }*/
-        for(int i=0; i<5; i++){
 
-                userList.add(staticNames[i]);
+        database.child("UserData").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for( DataSnapshot snapshot : dataSnapshot.getChildren()){
+                            User user = snapshot.getValue(User.class);
+                            if((user.password.equals(userPass))){
+                              currUser = user;
+                            }
+                        }
+                        for( DataSnapshot snapshot : dataSnapshot.getChildren()){
+                            User user = snapshot.getValue(User.class);
 
-        }/*
-        userArray = new String[counter];
-        for(int i=0; i< counter; i++){
-            userArray[i] = userList.get(i);
+                            if(!(user.name.equals(currUser.name)) && user.houseName.equals(currUser.houseName)){
+                                System.out.println(user.name);
+                                houseUserList.add(user);
+                                counter+=1;
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+        for (int i=0; i<counter; i++){
+            roomies.add(houseUserList.get(i).name);
         }
-        */
         ArrayAdapter<String> listArrayAdapter = new ArrayAdapter<String>(MainActivity.this,
-                android.R.layout.simple_list_item_1, userList);
+                android.R.layout.simple_list_item_1, roomies);
         if(listArrayAdapter != null) {
            listView.setAdapter(listArrayAdapter);
         }
 
-        messageStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, MessageActivity.class);
-                //intent.putExtra("CurrentUser", currUser.username);
-                startActivity(intent);
-            }
-        });
+
 
         goTodo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, ToDoActivity.class);
-               // intent.putExtra("CurrentUser", currUser.username);
+               // intent.putExtra("CurrentUser", currUser.userPass);
                 startActivity(intent);
             }
         });
 
-        displayMates.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(listView.getVisibility() == View.INVISIBLE){
-                    listView.setVisibility(View.VISIBLE);
-                }else{
-                    listView.setVisibility(View.INVISIBLE);
-                }
-                if(secondPress == false){
-                    view.getBackground().setColorFilter(0xe0f47521, PorterDuff.Mode.SRC_ATOP);
-                    view.invalidate();
-                    secondPress = true;
-                }else if(secondPress == true){
-                    view.getBackground().clearColorFilter();
-                    view.invalidate();
-                    secondPress = false;
-                }
-            }
-        });
+    }
+
+    public void StartMessaging(View view){
+            Intent intent = new Intent(MainActivity.this, MessageActivity.class);
+            intent.putExtra("CurrentUser", userPass);
+            startActivity(intent);
+    }
+
+    public void ShowRoomies(View view){
+        if(listView.getVisibility() == View.INVISIBLE){
+            listView.setVisibility(View.VISIBLE);
+        }else{
+            listView.setVisibility(View.INVISIBLE);
+        }
+        if(secondPress == false){
+            view.getBackground().setColorFilter(0xe0f47521, PorterDuff.Mode.SRC_ATOP);
+            view.invalidate();
+            secondPress = true;
+        }else if(secondPress == true){
+            view.getBackground().clearColorFilter();
+            view.invalidate();
+            secondPress = false;
+        }
     }
 
 
