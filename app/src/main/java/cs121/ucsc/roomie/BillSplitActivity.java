244d51cab2f3e72905e9614b9165d2f0ceeb23dc
justinse0 <carrier_mania@yahoo.com>
2017-11-26 15:18:18 -0800

    Venmo Activity

---
+++ app/src/main/java/cs121/ucsc/roomie/BillSplitActivity.java
@@ -0,0 +1,13 @@
+package cs121.ucsc.roomie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BillSplitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bill_split);
+    }
+}
