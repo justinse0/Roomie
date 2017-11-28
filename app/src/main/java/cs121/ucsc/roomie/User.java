4697202a1e78379fc7719f7d3d6a257a31172a42
yli186 <likevin111@gmail.com>
2017-11-27 19:44:33 -0800

    added venmoID to User

--- app/src/main/java/cs121/ucsc/roomie/User.java
+++ app/src/main/java/cs121/ucsc/roomie/User.java
@@ -9,14 +9,16 @@ public class User {
     public String houseName;
     public String password;
     public String houseAddress;
+    public String VenmoID;
     public User(){
 
     }
-    public User(String name, String houseName, String password, String houseAddress){
+    public User(String name, String houseName, String password, String houseAddress, String VenmoID){
         this.name = name;
         this.houseName = houseName;
         this.password = password;
         this.houseAddress = houseAddress;
+        this.VenmoID = VenmoID;
     }
 
 
