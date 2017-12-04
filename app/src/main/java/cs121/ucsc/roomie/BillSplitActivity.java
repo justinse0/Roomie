adcee9f4f4ee2432fdc497e4516b51bd079f081c
justinse0 <carrier_mania@yahoo.com>
2017-12-04 15:39:36 -0800

    update

--- app/src/main/java/cs121/ucsc/roomie/BillSplitActivity.java
+++ app/src/main/java/cs121/ucsc/roomie/BillSplitActivity.java
@@ -1,6 +1,7 @@
 package cs121.ucsc.roomie;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,9 +10,19 @@ public class BillSplitActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        System.out.println("fuckme");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bill_split);
     }
+
+    public void VenmoGo(View v){
+        System.out.println("fuckme");
+        Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.venmo");
+        if (launchIntent != null) {
+            System.out.println("fuckmee");
+            startActivity(launchIntent);//null pointer check in case package name was not found
+        }
+    }
+
+
 
 }
