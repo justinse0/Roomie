bb756e5dae13019417e3326a9b17018452fdbccd
yli186 <likevin111@gmail.com>
2017-12-05 21:32:29 -0800

    todo fixed

--- app/src/main/java/cs121/ucsc/roomie/ToDoActivity.java
+++ app/src/main/java/cs121/ucsc/roomie/ToDoActivity.java
@@ -9,9 +9,14 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 
@@ -20,22 +25,51 @@ public class ToDoActivity extends Activity {
     EditText editTextView;
     ArrayList<ToDoModel> ItemModelList;
     ToDoCustomAdapter customAdapter;
-    FirebaseAuth mAuth;
+
     DatabaseReference database;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do);
-
-        mAuth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance().getReference();
-
         listView = (ListView) findViewById(R.id.listview);
         editTextView = (EditText) findViewById(R.id.editTextView);
         ItemModelList = new ArrayList<ToDoModel>();
+
+
+        ItemModelList = fetch();
+
         customAdapter = new ToDoCustomAdapter(getApplicationContext(), ItemModelList);
+
         listView.setEmptyView(findViewById(R.id.listview));
         listView.setAdapter(customAdapter);
+        customAdapter.notifyDataSetChanged();
+
+    }
+    public ArrayList<ToDoModel> fetch(){
+        final ArrayList<ToDoModel> list = new ArrayList();
+        DatabaseReference dr = FirebaseDatabase.getInstance().getReference().child("ToDoData").child(MainActivity.currUser.houseName);
+        dr.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                try{
+                    for(DataSnapshot postSnapshot : dataSnapshot.getChildren()){
+                        ToDoModel tdm = postSnapshot.getValue(ToDoModel.class);
+                        System.out.println(tdm.getName());
+                        list.add(tdm);
+                    }
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        return list;
     }
     @SuppressLint("NewApi")
     public void addValue(View v) {
